109318ab1bb622c5e3823f6a5ecb2787011e004d
Neos Rafael Ruiz Sanchez <neos.ruiz@iesnervion.es>
2024-11-06 11:12:48 +0100

    aa

--- LecturaFicheroAleatorio/src/clases/TratamientoFichero.java
+++ LecturaFicheroAleatorio/src/clases/TratamientoFichero.java
@@ -7,12 +7,12 @@ import java.util.Iterator;
 
 public class TratamientoFichero {
 
-	public static final String RUTA_LEER = "src\\clases\\Lectura.txt";
+	public static final String RUTA_LEER = "src\\clases\\Lectura";
 	public static final String RUTA_ESCRIBIR = "src\\clases\\Escritura";
 
 	public static void main(String[] args) throws IOException {
 		crearArchivoLecturaAprtado1();
-		crearArchivoLecturaAprtado1();
+		escribirArchivoAprtado1();
 		crearArchivoLecturaAprtado2();
 		escribirArchivoInversoAprtado2();
 		crearArchivoLecturaAprtado3();
@@ -20,7 +20,7 @@ public class TratamientoFichero {
 	}
 
 	public static void crearArchivoLecturaAprtado1() throws IOException {
-		File ficheroLectura = new File(RUTA_LEER);
+		File ficheroLectura = new File(RUTA_LEER + "1.txt");
 
 		try (RandomAccessFile fileLeer = new RandomAccessFile(ficheroLectura, "rw")) {
 			fileLeer.writeBytes(("a"));
@@ -28,17 +28,16 @@ public class TratamientoFichero {
 	}
 
 	public static void escribirArchivoAprtado1() throws IOException {
-		File ficheroLectura = new File(RUTA_LEER);
-		File ficheroEscritura = new File(RUTA_ESCRIBIR+"1.txt");
+		File ficheroLectura = new File(RUTA_LEER + "1.txt");
+		File ficheroEscritura = new File(RUTA_ESCRIBIR + "1.txt");
 		char letra;
 		try (RandomAccessFile fileLeer = new RandomAccessFile(ficheroLectura, "r");
 				RandomAccessFile fileEscribir = new RandomAccessFile(ficheroEscritura, "rw")) {
-
 			fileLeer.seek(0);
 			letra = (char) fileLeer.readByte();
-			fileLeer.readByte();
+			
 			fileEscribir.seek(fileEscribir.length());
-			for (int i = 0; i < 4; i++) {
+			for (int i = 0; i < 5; i++) {
 				fileEscribir.writeBytes(letra + "");
 			}
 
@@ -46,36 +45,36 @@ public class TratamientoFichero {
 	}
 
 	public static void crearArchivoLecturaAprtado2() throws IOException {
-		File ficheroLectura = new File(RUTA_LEER);
+		File ficheroLectura = new File(RUTA_LEER + "2.txt");
 		try (RandomAccessFile fileLeer = new RandomAccessFile(ficheroLectura, "rw")) {
 			for (int i = 1; i <= 4; i++) {
 				fileLeer.writeBytes((char) ('a' + i - 1) + "\n");
-			
+
 			}
 		}
 	}
 
 	public static void escribirArchivoInversoAprtado2() throws IOException {
-	    File ficheroLectura = new File(RUTA_LEER);
-	    File ficheroEscritura = new File(RUTA_ESCRIBIR+"2.txt"); 
+		File ficheroLectura = new File(RUTA_LEER + "2.txt");
+		File ficheroEscritura = new File(RUTA_ESCRIBIR + "2.txt");
 
-	    char letra;
-	    try (RandomAccessFile fileLeer = new RandomAccessFile(ficheroLectura, "r");
-	         RandomAccessFile fileEscribir = new RandomAccessFile(ficheroEscritura, "rw")) {
-	       
-	        long posicion = fileLeer.length() - 2;
-	        while (posicion >= 0) {
-	            fileLeer.seek(posicion);
-	            letra = (char) fileLeer.readByte();
-	            fileEscribir.seek(fileEscribir.length()); 
-	            fileEscribir.writeBytes(letra + "\n");
-	            posicion -= 2;
-	        }
-	    }
+		char letra;
+		try (RandomAccessFile fileLeer = new RandomAccessFile(ficheroLectura, "r");
+				RandomAccessFile fileEscribir = new RandomAccessFile(ficheroEscritura, "rw")) {
+
+			long posicion = fileLeer.length() - 2;
+			while (posicion >= 0) {
+				fileLeer.seek(posicion);
+				letra = (char) fileLeer.readByte();
+				fileEscribir.seek(fileEscribir.length());
+				fileEscribir.writeBytes(letra + "\n");
+				posicion -= 2;
+			}
+		}
 	}
 
 	public static void crearArchivoLecturaAprtado3() throws IOException {
-		File ficheroLectura = new File(RUTA_LEER);
+		File ficheroLectura = new File(RUTA_LEER + "3.txt");
 		try (RandomAccessFile fileLeer = new RandomAccessFile(ficheroLectura, "rw")) {
 			fileLeer.writeBytes(("a1" + "\n"));
 			for (int i = 1; i <= 4; i++) {
@@ -85,8 +84,8 @@ public class TratamientoFichero {
 	}
 
 	public static void escribirArchivoInversoAprtado3() throws IOException {
-		File ficheroLectura = new File(RUTA_LEER);
-		File ficheroEscritura = new File(RUTA_ESCRIBIR+"3.txt");
+		File ficheroLectura = new File(RUTA_LEER + "3.txt");
+		File ficheroEscritura = new File(RUTA_ESCRIBIR + "3.txt");
 		long posicion;
 		char letra;
 		char numero;
